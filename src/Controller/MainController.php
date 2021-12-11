61b30f94b9f6bd91b1d43a6f295fc0c2af8136ab
Nathan DEMURU <blakei.57@icloud.com>
2021-12-11 10:58:38 +0100

    héritages et templates

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,30 +1,33 @@
 <?php
 
-namespace App\Controller;
+  namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+  use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+  use Symfony\Component\HttpFoundation\Response;
+  use Symfony\Component\Routing\Annotation\Route;
 
-class MainController extends AbstractController
-{
-    #[Route('/', name: 'main_homepage' , methods: ['GET'])]
-    public function homepage(): Response
-    {
-        return $this->render('main/homepage.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+  class MainController extends AbstractController {
+
+    #[Route('/', name: 'main_homepage', methods: ['GET'])]
+    public function homepage(): Response {
+      return $this->render('main/homepage.html.twig', [
+        'controller_name' => 'MainController',
+      ]);
     }
 
-    #[Route('/presentation', name: 'main_presentation' , methods: ['GET'])]
-    public function listProducts(): Response
-    {
-        return $this->render('main/presentation.html.twig');
+    #[Route('/presentation', name: 'main_presentation', methods: ['GET'])]
+    public function listProducts(): Response {
+      return $this->render('main/presentation.html.twig');
     }
 
-  #[Route('/contact', name: 'main_contact')]
-  public function contact(): Response {
-    return $this->render('main/contact.html.twig');
+    #[Route('/contact', name: 'main_contact')]
+    public function contact(): Response {
+      return $this->render('main/contact.html.twig');
+    }
+
+    #[Route('/produits-liste', name: 'main_product-list')]
+    public function productList(): Response {
+      return $this->render('main/productList.html.twig');
+    }
+
   }
-
-}
